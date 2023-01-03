63374dd118e08e5bee01114b35e2a943f7451439
ablecp <tasong12@kookmin.ac.kr>
2023-01-03 17:51:02 +0900

    feature:recycler view

--- nBread/app/src/main/java/com/mfiletasong/nbread/MainActivity.java
+++ nBread/app/src/main/java/com/mfiletasong/nbread/MainActivity.java
@@ -1,14 +1,41 @@
 package com.mfiletasong.nbread;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Triplist> dataList;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Triplist> recordItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // 리사이클러뷰 사이즈를 고정
+        recyclerView.setHasFixedSize(true);
+
+        // linear layout 사용
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Adapter와 엽동
+        mAdapter = new TriplistAdapter(recordItems);
+        recyclerView.setAdapter(mAdapter);
     }
 }
---
+++ nBread/app/src/main/java/com/mfiletasong/nbread/Triplist.java
@@ -0,0 +1,49 @@
+package com.mfiletasong.nbread;
+
+public class Triplist {
+    private int imageResourceID;
+    private String tripTitle;
+    private String duration;
+    private String member;
+
+    public Triplist(int id, String title, String duration, String member)
+    {
+        this.imageResourceID = id;
+        this.tripTitle = title;
+        this.duration = duration;
+        this.member = member;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public String getTripTitle(){
+        return tripTitle;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+
+    public void setTripTitle(String tripTitle) {
+        this.tripTitle = tripTitle;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
+
+}
